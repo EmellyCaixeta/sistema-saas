4d18e5be89944b68608264b617ccc8c29d8b7253
Emelly Vitoria <139073761+EmellyCaixeta@users.noreply.github.com>
2026-05-23 22:41:24 -0300

    Adicionando persistencia com localStorage

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+window.onload = function(){
+    mostrarTarefas();
+};
+
+function salvarLocalStorage(){
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
+
 function adicionarTarefa(){
 
     let input = document.getElementById("tarefa");
@@ -9,39 +19,78 @@ function adicionarTarefa(){
         return;
     }
 
-    let lista = document.getElementById("lista");
+    tarefas.push({
+        texto: texto,
+        concluida: false
+    });
 
-    let li = document.createElement("li");
+    salvarLocalStorage();
 
-    let span = document.createElement("span");
-    span.innerText = texto;
-
-    let divAcoes = document.createElement("div");
-    divAcoes.classList.add("acoes");
-
-    let btnConcluir = document.createElement("button");
-    btnConcluir.innerText = "✓";
-    btnConcluir.classList.add("btn-concluir");
-
-    btnConcluir.onclick = function(){
-        span.classList.toggle("concluida");
-    };
-
-    let btnRemover = document.createElement("button");
-    btnRemover.innerText = "X";
-    btnRemover.classList.add("btn-remover");
-
-    btnRemover.onclick = function(){
-        li.remove();
-    };
-
-    divAcoes.appendChild(btnConcluir);
-    divAcoes.appendChild(btnRemover);
-
-    li.appendChild(span);
-    li.appendChild(divAcoes);
-
-    lista.appendChild(li);
+    mostrarTarefas();
 
     input.value = "";
+}
+
+function mostrarTarefas(){
+
+    let lista = document.getElementById("lista");
+
+    lista.innerHTML = "";
+
+    tarefas.forEach(function(tarefa, index){
+
+        let li = document.createElement("li");
+
+        let span = document.createElement("span");
+
+        span.innerText = tarefa.texto;
+
+        if(tarefa.concluida){
+            span.classList.add("concluida");
+        }
+
+        let divAcoes = document.createElement("div");
+
+        divAcoes.classList.add("acoes");
+
+        let btnConcluir = document.createElement("button");
+
+        btnConcluir.innerText = "✓";
+
+        btnConcluir.classList.add("btn-concluir");
+
+        btnConcluir.onclick = function(){
+
+            tarefas[index].concluida = !tarefas[index].concluida;
+
+            salvarLocalStorage();
+
+            mostrarTarefas();
+        };
+
+        let btnRemover = document.createElement("button");
+
+        btnRemover.innerText = "X";
+
+        btnRemover.classList.add("btn-remover");
+
+        btnRemover.onclick = function(){
+
+            tarefas.splice(index, 1);
+
+            salvarLocalStorage();
+
+            mostrarTarefas();
+        };
+
+        divAcoes.appendChild(btnConcluir);
+
+        divAcoes.appendChild(btnRemover);
+
+        li.appendChild(span);
+
+        li.appendChild(divAcoes);
+
+        lista.appendChild(li);
+    });
 }
